81b7340205e95a1bb938c17d8d3d5859782930c3
Dhruva Vinod <dhruvavinod@gmail.com>
2026-01-11 13:44:08 +0530

    trying again :(

--- packages/nextjs/app/seller/page.tsx
+++ packages/nextjs/app/seller/page.tsx
@@ -54,7 +54,7 @@ const SellerDashboard: NextPage = () => {
     try {
       await listInvoice({
         functionName: "listInvoice",
-        args: [BigInt(tokenId), parseEther(price), BigInt(0)],
+        args: [BigInt(tokenId), parseEther(price), false],
       });
       alert("Listing Successful! Your invoice is on the market.");
     } catch (e) {
